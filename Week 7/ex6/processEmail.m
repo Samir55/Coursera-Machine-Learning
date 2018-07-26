1ee8c8f95ba82db0c5f7216786df85e9c453e046
Samir55 <discere044@icloud.com>
2018-07-26 02:34:26 +0200

    solving ex6.

--- Week 7/ex6/processEmail.m
+++ Week 7/ex6/processEmail.m
@@ -13,7 +13,7 @@ vocabList = getVocabList();
 word_indices = [];
 
 % ========================== Preprocess Email ===========================
-
+# USING REGEX SAMIR55
 % Find the Headers ( \n\n and remove )
 % Uncomment the following lines if you are working with raw emails with the
 % full headers
@@ -96,6 +96,12 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
+    for i =1:size(vocabList)(1)
+      if strcmp(vocabList(i), str) == 1
+        word_indices = [word_indices i]
+      endif
+    endfor
+    
 
 
 
